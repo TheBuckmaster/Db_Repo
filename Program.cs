7d700f5bdd98cbd7402c8cfc42fff51139ccbb20
Benjamin Buckmaster <bpbuckmaster@coe.edu>
2012-10-02 15:57:59 -0500

    Initial Commit
    Brought over files from old repo.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form1 OurForm = new Form1();
+            OurForm.CourseList = new List<string>();
+            OurForm.CourseList.Add("This is where a list would go"); 
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(OurForm);
+        }
+    }
+}
+
