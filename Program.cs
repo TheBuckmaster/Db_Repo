585f3a7bed51a682832af208a874ccd2b68c51f1
Benjamin Buckmaster <bpbuckmaster@coe.edu>
2012-10-03 02:10:20 -0500

    Program Passes Initial Course Data to Form1

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ namespace WindowsFormsApplication1
             Application.SetCompatibleTextRenderingDefault(false);                        
             Form1 OurForm = new Form1();
 
-            Application.Run(OurForm);
+            
             try
             {
-                using (StreamReader sr = new StreamReader("input file goes here"))
+                using (StreamReader sr = new StreamReader("UserInput.txt"))
                 {
                     String line = sr.ReadLine();
 
@@ -80,21 +80,78 @@ namespace WindowsFormsApplication1
             
 
 
-            string filename = "Classinput.txt";
+            string filename = "ClassInput.txt";
             if (File.Exists(filename))
             {
-                int localcharred = 0;
-                FileStream input = new FileStream(filename, FileMode.Open, FileAccess.Read);
-                StreamReader filereader = new StreamReader(input); 
+                int howmanycourses = 0;
+                string currstring;
+                string coursename;
+                string coursetitle;
+                string instructor;
+                float credit;
+                int seats;
+                int timeblocks;
+                Stack<string> times;
                 
+                FileStream input = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                StreamReader filereader = new StreamReader(input);
+                try
+                {
+                    
+                    while (!filereader.EndOfStream)
+                    {
+                        currstring = filereader.ReadLine();
+                        coursename = currstring.Substring(0, 10);
+                        currstring.Remove(0, 10);
+                        currstring.TrimStart();
+                        coursetitle = currstring.Substring(0, 15);
+                        currstring.Remove(0, 15);
+                        currstring.TrimStart();
+                        instructor = currstring.Substring(0, 10);
+                        currstring.Remove(0, 10);
+                        currstring.TrimStart();
+                        credit = float.Parse(currstring.Substring(0, 4));
+                        currstring.Remove(0, 4);
+                        currstring.TrimStart();
+                        seats = int.Parse(currstring.Substring(0, 3));
+                        currstring.Remove(0, 3);
+                        currstring.TrimStart();
+                        timeblocks = int.Parse(currstring.Substring(0, 1));
+                        currstring.Remove(0, 1);
+                        currstring.TrimStart();
+
+                        times = new Stack<string>();
+                        for (int counter = 0; counter < timeblocks; counter++)
+                        {
+                            string littlestring = currstring.Substring(0, 5);
+                            times.Push(littlestring);
+                            currstring.Remove(0, 5);
+                            currstring.TrimStart();
+                        }
+
+                        OurForm.Courses[howmanycourses].coursename = coursename;
+                        OurForm.Courses[howmanycourses].coursetitle = coursetitle;
+                        OurForm.Courses[howmanycourses].instructor = instructor;
+                        OurForm.Courses[howmanycourses].credit = credit;
+                        OurForm.Courses[howmanycourses].seats  = seats;
+                        OurForm.Courses[howmanycourses].timeblocks = timeblocks;
+                        OurForm.Courses[howmanycourses].times = times;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid Input File");
+                    Console.WriteLine(e.Message);
+                }
             }
             else
             {
-                //Invalid File Configuration. 
+                Console.WriteLine("Your File does not Exist."); 
             }
             
             OurForm.CourseList = new List<string>();
-            OurForm.CourseList.Add("This is where a list would go"); 
+            OurForm.CourseList.Add("This is where a list would go");
+            Application.Run(OurForm);
         }
     }
 }
