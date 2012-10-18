ada66315f959d8cf39773b5aecad27b4d5f60ba8
Avery Podell-Blume <apblume@coe.edu>
2012-10-17 20:36:06 -0500

    Courseinfo now section specific

--- Courseinfo.cs
+++ Courseinfo.cs
@@ -8,16 +8,16 @@ public class courseinfo
     private string instructor;
     private float credit;
     private int seats;
-    private int timeblocks;  // how many timeblocks? 
-    private Stack<string> times; // as initialized, these are ddttks in reverse order of entry.
+    private int enrolled;
+    private List<int> times; // as initialized, these are ddttks in reverse order of entry.
 
     public string Coursename { get { return coursename; } }
     public string Coursetitle { get { return coursetitle; } }
     public string Instructor { get { return instructor; } }
     public float Credit { get { return credit; } }
     public int Seats { get { return seats; } }
-    public int Timeblocks { get { return timeblocks; } }
-    public Stack<string> Times { get { return times; } }
+    public int Enrolled { get { return enrolled; } set { enrolled = Enrolled; } }
+    public List<int> Times { get { return times; } }
 
 	public courseinfo(string name, string title, string prof, float cred, int spots, int timeblks, Stack<string> timestack)
 	{   
@@ -30,5 +30,4 @@ public class courseinfo
         times = timestack;
 	}
 
-    public void addTime(string time) { times.Push(time); }
 }
