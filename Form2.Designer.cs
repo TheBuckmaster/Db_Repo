4608165e9f3d0b861cbfda580041fe689e6ede10
Chris Edwards <cmedwards@coe.edu>
2012-10-17 21:55:41 -0500

    Made a new form for students
    Need some Ben to show me how the course stuff works.

---
+++ Form2.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showScheduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewScheduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CheckBoxes = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.CrsTtle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CrsName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prof = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seets = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Credt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dys = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tmie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(697, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showScheduleToolStripMenuItem,
+            this.viewScheduleToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // showScheduleToolStripMenuItem
+            // 
+            this.showScheduleToolStripMenuItem.Name = "showScheduleToolStripMenuItem";
+            this.showScheduleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showScheduleToolStripMenuItem.Text = "Add Courses";
+            // 
+            // viewScheduleToolStripMenuItem
+            // 
+            this.viewScheduleToolStripMenuItem.Name = "viewScheduleToolStripMenuItem";
+            this.viewScheduleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.viewScheduleToolStripMenuItem.Text = "View Schedule";
+            this.viewScheduleToolStripMenuItem.Click += new System.EventHandler(this.viewScheduleToolStripMenuItem_Click);
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CheckBoxes,
+            this.CrsTtle,
+            this.CrsName,
+            this.Prof,
+            this.seets,
+            this.Credt,
+            this.Dys,
+            this.Tmie});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 87);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(673, 275);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // CheckBoxes
+            // 
+            this.CheckBoxes.Frozen = true;
+            this.CheckBoxes.HeaderText = "";
+            this.CheckBoxes.Name = "CheckBoxes";
+            this.CheckBoxes.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.CheckBoxes.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.CheckBoxes.Width = 50;
+            // 
+            // CrsTtle
+            // 
+            this.CrsTtle.Frozen = true;
+            this.CrsTtle.HeaderText = "Course Title";
+            this.CrsTtle.MinimumWidth = 50;
+            this.CrsTtle.Name = "CrsTtle";
+            this.CrsTtle.ReadOnly = true;
+            this.CrsTtle.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.CrsTtle.Width = 50;
+            // 
+            // CrsName
+            // 
+            this.CrsName.Frozen = true;
+            this.CrsName.HeaderText = "Course Name";
+            this.CrsName.MinimumWidth = 150;
+            this.CrsName.Name = "CrsName";
+            this.CrsName.ReadOnly = true;
+            this.CrsName.Width = 150;
+            // 
+            // Prof
+            // 
+            this.Prof.HeaderText = "Professer";
+            this.Prof.Name = "Prof";
+            this.Prof.ReadOnly = true;
+            this.Prof.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // seets
+            // 
+            this.seets.HeaderText = "Seats";
+            this.seets.Name = "seets";
+            this.seets.Width = 50;
+            // 
+            // Credt
+            // 
+            this.Credt.HeaderText = "Credit";
+            this.Credt.Name = "Credt";
+            this.Credt.Width = 25;
+            // 
+            // Dys
+            // 
+            this.Dys.HeaderText = "Date";
+            this.Dys.Name = "Dys";
+            this.Dys.ReadOnly = true;
+            this.Dys.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Tmie
+            // 
+            this.Tmie.HeaderText = "Time";
+            this.Tmie.Name = "Tmie";
+            this.Tmie.ReadOnly = true;
+            this.Tmie.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showScheduleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewScheduleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn CheckBoxes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CrsTtle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CrsName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prof;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seets;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Credt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tmie;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewScheduleToolStripMenuItem_Click(object sender, EventArgs e)//Student, tries to put the stuff in the data
+            //view
+        {
+            int x;
+
+            foreach (DataGridViewRow row in this.dataGridView1.Rows)
+            {
+                DataGridViewCell cell = row.Cells[x];  
+                if (cell.Value == null || cell.Value.Equals(""))
+                {
+                    continue;
+                }
+            }
+
+             // my attempt to make it workish
+            for (x = 0; x < CourseList.Count; x++)
+            {
+                int i = 1; //starts at course Title then moves on
+                cell[i].Value = CourseList[x].Coursetitle;
+                i++;
+                cell[i].Value = CourseList[x].Coursename;
+                i++;
+                cell[i].Value = CourseList[x].Instructor;
+                i++;
+                cell[i].Value = CourseList[x].credit;
+                i++;
+                cell[i].Value = CourseList[x].seats;
+                i++;
+                cell[i].Value = CourseList[x].timestack;
+                i++;
+                cell[i].Value = CourseList[x].timeblks;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form Form1 = new Form();
+            Form1.Show();
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    }
+}
