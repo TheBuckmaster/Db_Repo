2962ca42e93fa9b8996705a3a31eadfc25e4db9b
Chris Edwards <cmedwards@coe.edu>
2012-10-21 23:40:32 -0500

    Form3
    Actually loads up!!

---
+++ Form3.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddCourseBttn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CheckBoxes = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.CrsTtle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CrsName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prof = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seets = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Credt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dys = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tmie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCourseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteCourseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewScheduleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AddCourseBttn
+            // 
+            this.AddCourseBttn.Location = new System.Drawing.Point(619, 326);
+            this.AddCourseBttn.Name = "AddCourseBttn";
+            this.AddCourseBttn.Size = new System.Drawing.Size(75, 23);
+            this.AddCourseBttn.TabIndex = 5;
+            this.AddCourseBttn.Text = "Add Course";
+            this.AddCourseBttn.UseVisualStyleBackColor = true;
+            this.AddCourseBttn.Visible = false;
+            this.AddCourseBttn.Click += new System.EventHandler(this.AddCourseBttn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CheckBoxes,
+            this.CrsTtle,
+            this.CrsName,
+            this.Prof,
+            this.seets,
+            this.Credt,
+            this.Dys,
+            this.Tmie});
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 275);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Visible = false;
+            // 
+            // CheckBoxes
+            // 
+            this.CheckBoxes.Frozen = true;
+            this.CheckBoxes.HeaderText = "";
+            this.CheckBoxes.Name = "CheckBoxes";
+            this.CheckBoxes.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.CheckBoxes.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.CheckBoxes.Width = 50;
+            // 
+            // CrsTtle
+            // 
+            this.CrsTtle.Frozen = true;
+            this.CrsTtle.HeaderText = "Course Title";
+            this.CrsTtle.MinimumWidth = 50;
+            this.CrsTtle.Name = "CrsTtle";
+            this.CrsTtle.ReadOnly = true;
+            this.CrsTtle.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.CrsTtle.Width = 50;
+            // 
+            // CrsName
+            // 
+            this.CrsName.Frozen = true;
+            this.CrsName.HeaderText = "Course Name";
+            this.CrsName.MinimumWidth = 150;
+            this.CrsName.Name = "CrsName";
+            this.CrsName.ReadOnly = true;
+            this.CrsName.Width = 150;
+            // 
+            // Prof
+            // 
+            this.Prof.HeaderText = "Professer";
+            this.Prof.Name = "Prof";
+            this.Prof.ReadOnly = true;
+            this.Prof.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // seets
+            // 
+            this.seets.HeaderText = "Seats";
+            this.seets.Name = "seets";
+            this.seets.Width = 50;
+            // 
+            // Credt
+            // 
+            this.Credt.HeaderText = "Credit";
+            this.Credt.Name = "Credt";
+            this.Credt.Width = 40;
+            // 
+            // Dys
+            // 
+            this.Dys.HeaderText = "Date";
+            this.Dys.Name = "Dys";
+            this.Dys.ReadOnly = true;
+            this.Dys.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Tmie
+            // 
+            this.Tmie.HeaderText = "Time";
+            this.Tmie.Name = "Tmie";
+            this.Tmie.ReadOnly = true;
+            this.Tmie.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCourseToolStripMenuItem,
+            this.deleteCourseToolStripMenuItem,
+            this.viewScheduleToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // addCourseToolStripMenuItem
+            // 
+            this.addCourseToolStripMenuItem.Name = "addCourseToolStripMenuItem";
+            this.addCourseToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.addCourseToolStripMenuItem.Text = "Add Course";
+            // 
+            // deleteCourseToolStripMenuItem
+            // 
+            this.deleteCourseToolStripMenuItem.Name = "deleteCourseToolStripMenuItem";
+            this.deleteCourseToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.deleteCourseToolStripMenuItem.Text = "Delete Course";
+            // 
+            // viewScheduleToolStripMenuItem
+            // 
+            this.viewScheduleToolStripMenuItem.Name = "viewScheduleToolStripMenuItem";
+            this.viewScheduleToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.viewScheduleToolStripMenuItem.Text = "View Schedule";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 361);
+            this.Controls.Add(this.AddCourseBttn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddCourseBttn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn CheckBoxes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CrsTtle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CrsName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prof;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seets;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Credt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tmie;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCourseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteCourseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewScheduleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        List<courseinfo> CourseList = new List<courseinfo>();
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        public Form3(List<courseinfo> cl)
+        {
+            InitializeComponent();
+            CourseList = cl;
+            dataGridView1.DataSource = CourseList;
+        }
+
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewScheduleToolStripMenuItem_Click(object sender, EventArgs e)//Student, tries to put the stuff in the data
+            //view
+        {
+            dataGridView1.Visible = true;
+            dataGridView1.Enabled = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form Form1 = new Form();
+            Form1.Show();
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+               int selectedCellCount =
+        dataGridView1.GetCellCount(DataGridViewElementStates.Selected);
+            if (selectedCellCount > 0)
+         {
+                
+         }
+            else
+            MessageBox.Show("Select a course to add a course");
+        }
+
+        private void deleteCourseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addCourseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddCourseBttn.Enabled = true;
+            AddCourseBttn.Visible = true;
+            dataGridView1.Enabled = true;
+            dataGridView1.Visible = true;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+    }
+}
