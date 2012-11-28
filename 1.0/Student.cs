88ac6efb8ffff5f7d408f3cdbce44a6b950d2309
Avery <apblume@coe.edu>
2012-11-28 15:02:11 -0600

    student class

---
+++ 1.0/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Student
+{ 
+    public string UserName;
+    public string Password;
+    public string FirstName;
+    public string MiddleName;
+    public string LastName;
+    public string Status;
+
+    public List<string> Next = new List<courseinfo>();
+    public List<pastcourse> Current = new List<pastcourse>();
+    public List<pastcourse> History = new List<pastcourse>();
+    public List<string> Conflicts = new List<string>();
+
+    public Student(string uname, string pswrd, string fname, string mname, string lname, string stat)
+    {
+        UserName = uname;
+        Password = pswrd;
+        FirstName = fname;
+        MiddleName = mname;
+        LastName = lname;
+        Status = stat;
+    }
+
+    public double enrolledCredits()
+    {
+        double credits = 0.00;
+        foreach (courseinfo course in Next)
+            credits += course.Credit;
+        return credits;
+    }
+
+    public double earnedCredits()
+    {
+        double credits = 0.00;
+        foreach (pastcourse course in History)
+            credits += course.Credit;
+        return credits;
+    }
+
+    public double gpa()
+    {
+
+    }
+}
