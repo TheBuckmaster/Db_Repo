e2609c432a509f27ca9e20e6c451589b98da2fef
Benjamin Buckmaster <bpbuckmaster@coe.edu>
2012-11-28 14:56:10 -0600

    My Course, etc.

---
+++ 1.0/FirstPileofCourses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+
+    
+
+    public class Course
+    {
+        public String CourseTitle {get; set;}
+        public String CourseName { get; set;}
+        List<String> Meetings;
+        List<String> Students; //List of Student Usernames
+        String Teacher = "Staff"; 
+
+        public Course()
+        {
+            CourseTitle = "No Title";
+            CourseName = "No Name"; 
+            Meetings = new List<String>();
+            Students = new List<String>();
+
+        }
+
+        Course(String Title, String Name);
+        {
+    
+
+
+        }
+
+
+
+
+    }
+
+
+    class PastCourse
+    {
+ 
+
+    }
+
+
+
+}
