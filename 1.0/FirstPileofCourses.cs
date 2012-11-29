9cbabd8b1c33d91b0bcc390144638f36e4483c80
Benjamin Buckmaster <bpbuckmaster@coe.edu>
2012-11-29 00:42:10 -0600

    Course and Course Functionality Library
    Type Course and Type PastCourse will be in this file, which will be
    renamed at a later point. I am also writing here a library for methods
    that are more general than Course/PastCourse Oriented.

--- 1.0/FirstPileofCourses.cs
+++ 1.0/FirstPileofCourses.cs
@@ -5,32 +5,171 @@ using System.Text;
 
 namespace WindowsFormsApplication1
 {
+    //Administrative Library of Course and Program-Related Helper Functions
+    public static class CourseAdmin
+    {
+        /// <summary>
+        /// Reads in input from the named file. Adds to existing lists with content from file. 
+        /// </summary>
+        /// <param name="filename"> The name of a local file to read from. </param>
+        //public static void readDB(string filename,
+        //    List<Student> s,// List<Faculty> f, List<Admins> a,
+        //    List<Course> courses) 
+        //{
+        
+            
 
+
+        //}
+
+        public static String MeetingDecoder(String MeetingTime)
+        {
+            StringBuilder OutString = new StringBuilder();
+
+            int dd = Convert.ToInt32(MeetingTime.Substring(0, 2), 10);
+            int tt = Convert.ToInt32(MeetingTime.Substring(2, 2), 10);
+            int l = Convert.ToInt32(MeetingTime.Substring(4, 1), 10);
+
+            //day decode
+            if ((dd & 1) == 1)
+                OutString.Append("M");
+            if ((dd & 2) == 2)
+                OutString.Append("T");
+            if ((dd & 4) == 4)
+                OutString.Append("W");
+            if ((dd & 8) == 8)
+                OutString.Append("H");
+            if ((dd & 16) == 16)
+                OutString.Append("F");
+
+            OutString.Append("  "); 
+            //time decode
+            String StartTime = "10:00 AM";
+            String EndTime = "11:00 AM";
+
+            //Actual decode here. 
+            
+            OutString.Append(StartTime);
+            OutString.Append("-");
+            OutString.Append(EndTime); 
+            
+            return OutString.ToString(); 
+        }
+
+    
+    }
     
 
     public class Course
     {
-        public String CourseTitle {get; set;}
-        public String CourseName { get; set;}
-        List<String> Meetings;
-        List<String> Students; //List of Student Usernames
-        String Teacher = "Staff"; 
+        private String coursetitle;
+        private String coursename;
+        private String teacher = "Staff";
+        private int maxSeats;
+        private List<String> students; //List of Student Usernames
+        private List<String> Meetings = new List<String>();
+        private List<String> PreReqs = new List<String>();
 
+
+        public String CourseTitle
+        {
+            get 
+            { 
+                return coursetitle; 
+            } 
+        }
+        public String CourseName 
+        { 
+            get 
+            { 
+                return coursename; 
+            } 
+        }
+        public String Teacher 
+        {
+            get
+            {
+                return teacher;
+            }
+        }
+        public int MaxSeats 
+        {
+            get
+            {
+                return maxSeats;
+            }
+        }
+        public int CurrSeats
+        {
+            get
+            {
+                return students.Count;
+            }
+        }
+        public bool Full
+        {
+            get
+            {
+                return (CurrSeats > MaxSeats);
+            }
+        }
+        public List<String> Students
+        {
+            get
+            {
+                return students;
+            }
+        }     
+        public int numMeetings
+        {
+            get
+            {
+                return Meetings.Count;
+            }
+        }
+        
+        
+        
         public Course()
         {
-            CourseTitle = "No Title";
-            CourseName = "No Name"; 
+            coursetitle = "No Title";
+            coursename = "No Name"; 
             Meetings = new List<String>();
-            Students = new List<String>();
+            students = new List<String>();
 
         }
 
-        Course(String Title, String Name);
+        public Course(String Title, String Name, List<String> propMeetings)
         {
     
-
+            coursetitle = Title;
+            coursename = Name;
+            Meetings = new List<String>(propMeetings);
+            students = new List<String>();
 
         }
+
+        public Course(String Title, String Name, List<String> propMeetings, String fac)
+        {
+            coursetitle = Title;
+            coursename = Name;
+            Meetings = new List<String>(propMeetings);
+            students = new List<String>();
+            teacher = fac; 
+        
+        }
+
+        public List<String> MeetingTimes()
+        {
+            List<String> OutList = new List<String>(); 
+
+            foreach (String time in Meetings)
+                OutList.Add(CourseAdmin.MeetingDecoder(time)); 
+
+            return OutList; 
+        }
+
+
 
 
 
