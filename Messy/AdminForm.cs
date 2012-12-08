33f978cb9f2bbc52cfc73862c982ecb94d6ae782
unknown <apblume@SH308-16.coe.int>
2012-12-07 20:32:53 -0600

    admin form bones - avery

---
+++ Messy/AdminForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BensCRS
+{
+    public partial class AdminForm : Form
+    {
+        UserAdmin admin;
+        List<UserFaculty> FacultyList = new List<UserFaculty>();
+        List<UserStudent> StudentList = new List<UserStudent>();
+        List<Course> Courses = new List<Course>();
+
+        public AdminForm(List<UserFaculty> fl, List<UserStudent> sl, List<Course> cl, UserAdmin adm)
+        {
+            InitializeComponent();
+            admin = adm;
+            FacultyList = fl;
+            StudentList = sl;
+            Courses = cl;
+        }
+
+
+
+        // ?????????????
+        //private void AdminForm_Load(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+}
